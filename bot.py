c297c2ea85293d4425b6e1a7caa4f7ce0a02a596
pechenzz <59606818+pechenzz@users.noreply.github.com>
2020-08-15 17:13:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,15 +63,31 @@ async def invite(ctx):
 	embed=discord.Embed(title="Invite bot", url='https://discord.com/api/oauth2/authorize?client_id=736665323159289897&permissions=2048&scope=bot', color=random.randint(0, 0xFFFFFF))
 	embed.set_footer(text="Bot by yourordinaryred#2736 | Scenarios from https://creativichee.tumblr.com/miniscenario")
 	await ctx.send(embed=embed)
+	
+@bot.command()
+async def ping(ctx):
+	msgs = {
+	['Pong!'],
+	[f"{ctx.message.author.mention}. You asked for it, so don't complain!"],
+	['Someone except the owner actually uses it?'],
+	['I, EvaX, humbly subm- Wait, wrong user!!'],
+	['Mi pan, zum zum zum, zum zum zum. Mi pan, yakasus, nan nan nan'],
+	['mkk'],
+	['haha ping go brr']}
+	
+	msgnumb = random.randint(0, len(msgs))
+	
+	print(f'{msgs[msgnumb]}: {bot.latency}ms')
 
-#help command
 @bot.command()
 async def help(ctx):
 	embed=discord.Embed(title="Help command", color=random.randint(0, 0xFFFFFF))
 	embed.add_field(name="sc!scenario <A> <B>", value='Generates a scenario with two persons you input. (If the persons name contains spaces, just surround it with "" brackets, example: sc!scenario "Person A" "Person B")', inline=False)
 	embed.add_field(name="sc!help", value="Sends this embed.", inline=False)
 	embed.add_field(name="sc!invite", value="Sends an invite for this bot.", inline=False)
+	embed.add_field(name="sc!ping", value="Shows the latency of the bot (The only command with humor).", inline=False)
 	embed.set_footer(text="Bot by yourordinaryred#2736 | Scenarios from https://creativichee.tumblr.com/miniscenario")
 	await ctx.send(embed=embed)
+
 
 bot.run(TOKEN)
